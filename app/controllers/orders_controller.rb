7d2d2339bb6b9dfeca60af1fceaf84965274829c
trevormast <trevormast@gmail.com>
2016-05-13 12:27:07 -0700

    Adds OrderNotifier mailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,6 +38,8 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
 
+        OrderNotifier.recieved(@order).deliver
+
         format.html { redirect_to store_url, notice: 'Thank you for your order' }
         format.json { render :show, status: :created, location: @order }
       else
