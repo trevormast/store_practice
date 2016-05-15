05ef427d147496c503cf7214b253f990f9efa7bd
trevormast <trevormast@gmail.com>
2016-05-14 21:37:17 -0700

    Limits access to non-admins

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+
+    def ensure_an_admin_remains
+      if User.count.zero?
+        # raising an error causes an automatic rollback
+        raise "Can't delete last user"
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,33 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-
-  get 'store/index'
-  resources :products do
-    get :who_bought, on: :member
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
   end
 
-  root 'store#index', as: 'store'
-
-  resources :products
-
+  resources :users
+  resources :orders
   resources :line_items do
     member do
       put 'decrement'
     end
   end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
+  resources :carts
+
+  get "store/index"
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'store#index', as: 'store'
+  # ...
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
